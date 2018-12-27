9502e1c482af31a4b70bfd7b8370044d1b14b19f
Bruno Custodio <bcustodio@mesosphere.io>
2018-12-27 13:32:09 -0100

    test: add e2e test for manual pool deletion
    Signed-off-by: Bruno Custodio <bcustodio@mesosphere.io>

--- test/e2e/service_test.go
+++ test/e2e/service_test.go
@@ -119,6 +119,10 @@ var _ = Describe("Service", func() {
 					return p == "PONG", nil
 				})
 				Expect(err).NotTo(HaveOccurred(), "timed out while attempting to ping redis")
+
+				// Manually delete the services now in order to prevent the EdgeLB pool from being re-created during namespace deletion.
+				err = f.KubeClient.CoreV1().Services(redisSvc.Namespace).Delete(redisSvc.Name, metav1.NewDeleteOptions(0))
+				Expect(err).NotTo(HaveOccurred(), "failed to delete service %q", kubernetes.Key(redisSvc))
 			})
 		})
 
@@ -282,6 +286,138 @@ var _ = Describe("Service", func() {
 					err = f.KubeClient.CoreV1().Services(mongoSvc.Namespace).Delete(mongoSvc.Name, metav1.NewDeleteOptions(0))
 					Expect(err).NotTo(HaveOccurred(), "failed to delete service %q", kubernetes.Key(mongoSvc))
 				})
+			})
+		})
+
+		It("requested for public exposure and whose pool has been manually deleted is correctly re-provisioned by EdgeLB [TCP] [Public]", func() {
+			// Create a temporary namespace for the current test.
+			f.WithTemporaryNamespace(func(namespace *corev1.Namespace) {
+				var (
+					err      error
+					pool     *models.V2Pool
+					redisPod *corev1.Pod
+					redisSvc *corev1.Service
+				)
+
+				// Create a pod running Redis.
+				redisPod, err = f.CreatePod(namespace.Name, "redis", func(pod *corev1.Pod) {
+					pod.ObjectMeta.Labels = map[string]string{
+						"app": "redis",
+					}
+					pod.Spec.Containers = []corev1.Container{
+						{
+							Name:  "redis",
+							Image: "redis:5.0.3",
+							Ports: []corev1.ContainerPort{
+								{
+									Name:          "redis",
+									Protocol:      corev1.ProtocolTCP,
+									ContainerPort: 6379,
+								},
+							},
+						},
+					}
+				})
+				Expect(err).NotTo(HaveOccurred(), "failed to create redis test pod")
+
+				// We'd like to map port 6379 to a different frontend bind port, so we build the required annotation beforehand.
+				portmapAnnotationKey := fmt.Sprintf("%s%d", constants.EdgeLBPoolPortMapKeyPrefix, redisPod.Spec.Containers[0].Ports[0].ContainerPort)
+
+				// Create a service of type LoadBalancer targeting the pod created above.
+				redisSvc, err = f.CreateServiceOfTypeLoadBalancer(namespace.Name, "redis", func(svc *corev1.Service) {
+					svc.ObjectMeta.Annotations = map[string]string{
+						// Request for the pool to be called "<namespace-name>".
+						constants.EdgeLBPoolNameAnnotationKey: namespace.Name,
+						// Request for the pool to be deployed to an agent with the "slave_public" role.
+						constants.EdgeLBPoolRoleAnnotationKey: constants.EdgeLBRolePublic,
+						// Request for the pool to be given 0.2 CPUs.
+						constants.EdgeLBPoolCpusAnnotationKey: "200m",
+						// Request for the pool to be given 256MiB of RAM.
+						constants.EdgeLBPoolMemAnnotationKey: "256Mi",
+						// Request for the pool to be deployed into a single agent.
+						constants.EdgeLBPoolSizeAnnotationKey: "1",
+						// Request for the 6379 port of the service to be mapped into the 16379 port.
+						portmapAnnotationKey: "16379",
+					}
+					svc.Spec.Ports = []corev1.ServicePort{
+						{
+							Name:       "redis",
+							Protocol:   corev1.ProtocolTCP,
+							Port:       redisPod.Spec.Containers[0].Ports[0].ContainerPort,
+							TargetPort: intstr.FromInt(int(redisPod.Spec.Containers[0].Ports[0].ContainerPort)),
+						},
+					}
+					svc.Spec.Selector = redisPod.ObjectMeta.Labels
+				})
+				Expect(err).NotTo(HaveOccurred(), "failed to create test service")
+
+				// Wait for EdgeLB to acknowledge the pool's creation.
+				err = retry.WithTimeout(framework.DefaultRetryTimeout, framework.DefaultRetryInterval, func() (bool, error) {
+					ctx, fn := context.WithTimeout(context.Background(), framework.DefaultRetryInterval/2)
+					defer fn()
+					pool, err = f.EdgeLBManager.GetPoolByName(ctx, redisSvc.Annotations[constants.EdgeLBPoolNameAnnotationKey])
+					return err == nil, nil
+				})
+				Expect(err).NotTo(HaveOccurred(), "timed out while waiting for the edgelb api server to acknowledge the pool's creation")
+
+				// Wait for Redis to be reachable.
+				err = retry.WithTimeout(framework.DefaultRetryTimeout, framework.DefaultRetryInterval, func() (bool, error) {
+					redisClient := redis.NewClient(&redis.Options{
+						Addr: fmt.Sprintf("%s:%s", publicIP, redisSvc.Annotations[portmapAnnotationKey]),
+						DB:   0,
+					})
+					p, _ := redisClient.Ping().Result()
+					return p == "PONG", nil
+				})
+				Expect(err).NotTo(HaveOccurred(), "timed out while attempting to ping redis")
+
+				// Set the type of "redisSvc" to "NodePort".
+				// This will cause the service controller to stop processing this resource, and will prevent the pool from being re-created too soon.
+				redisSvc.Spec.Type = corev1.ServiceTypeNodePort
+				redisSvc, err = f.KubeClient.CoreV1().Services(redisSvc.Namespace).Update(redisSvc)
+				Expect(err).NotTo(HaveOccurred(), "failed to set the type of %q to %q", kubernetes.Key(redisSvc), corev1.ServiceTypeNodePort)
+
+				// Delete the pool.
+				f.DeleteEdgeLBPool(pool)
+
+				// Wait for Redis to NOT be reachable (meaning the pool and all load-balancer instances have been effectively deleted).
+				err = retry.WithTimeout(framework.DefaultRetryTimeout, framework.DefaultRetryInterval, func() (bool, error) {
+					redisClient := redis.NewClient(&redis.Options{
+						Addr: fmt.Sprintf("%s:%s", publicIP, redisSvc.Annotations[portmapAnnotationKey]),
+						DB:   0,
+					})
+					_, err := redisClient.Ping().Result()
+					return err != nil, nil
+				})
+				Expect(err).NotTo(HaveOccurred(), "timed out while waiting for redis to become unreachable")
+
+				// Give the pool's framework scheduler some extra time to perform cleanup before causing the pool to be recreated.
+				// https://jira.mesosphere.com/browse/DCOS-46508
+				time.Sleep(30 * time.Second)
+
+				// Set the type of "redisSvc" back to "LoadBalancer".
+				// This will cause the service controller to restart processing this resource, and re-create the pool since we're using the default pool re-creation strategy.
+				redisSvc.Spec.Type = corev1.ServiceTypeLoadBalancer
+				redisSvc, err = f.KubeClient.CoreV1().Services(redisSvc.Namespace).Update(redisSvc)
+				Expect(err).NotTo(HaveOccurred(), "failed to set the type of %q to %q", kubernetes.Key(redisSvc), corev1.ServiceTypeNodePort)
+
+				// Wait for the pool to be re-provisioned, making Redis reachable again.
+				err = retry.WithTimeout(framework.DefaultRetryTimeout, framework.DefaultRetryInterval, func() (bool, error) {
+					redisClient := redis.NewClient(&redis.Options{
+						Addr: fmt.Sprintf("%s:%s", publicIP, redisSvc.Annotations[portmapAnnotationKey]),
+						DB:   0,
+					})
+					p, _ := redisClient.Ping().Result()
+					return p == "PONG", nil
+				})
+				Expect(err).NotTo(HaveOccurred(), "timed out while attempting to ping redis")
+
+				// Manually delete the two services now in order to prevent the EdgeLB pool from being re-created during namespace deletion.
+				err = f.KubeClient.CoreV1().Services(redisSvc.Namespace).Delete(redisSvc.Name, metav1.NewDeleteOptions(0))
+				Expect(err).NotTo(HaveOccurred(), "failed to delete service %q", kubernetes.Key(redisSvc))
+
+				// Delete the EdgeLB pool.
+				f.DeleteEdgeLBPool(pool)
 			})
 		})
 	})
