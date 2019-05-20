1d6a32a9be2c0d0b0b93346617fae0023caca392
Simão Reis <sreis@mesosphere.io>
2019-05-20 19:05:02 +0000

    tests: Make go race detector happy
    Signed-off-by: Simão Reis <sreis@mesosphere.io>

--- pkg/controllers/fakes.go
+++ pkg/controllers/fakes.go
@@ -1,8 +1,12 @@
 package controllers
 
-import "context"
+import (
+	"context"
+	"sync"
+)
 
 type fakeGenericController struct {
+	mutex        *sync.Mutex
 	runError     error
 	enqueueError error
 	queue        []interface{}
@@ -11,6 +15,7 @@ type fakeGenericController struct {
 
 func newFakeGenericController() *fakeGenericController {
 	return &fakeGenericController{
+		mutex:     &sync.Mutex{},
 		queue:     make([]interface{}, 0),
 		tombstone: make([]interface{}, 0),
 	}
@@ -21,9 +26,13 @@ func (c *fakeGenericController) Run(ctx context.Context) error {
 }
 
 func (c *fakeGenericController) enqueue(obj interface{}) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
 	c.queue = append(c.queue, obj)
 }
 
 func (c *fakeGenericController) enqueueTombstone(obj interface{}) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
 	c.tombstone = append(c.tombstone, obj)
 }
