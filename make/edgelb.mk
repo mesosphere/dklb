76c9ee17cd84ccb9e27df17e5cfcb62763f808cd
Simão Reis <sreis@mesosphere.io>
2019-06-27 09:09:34 +0000

    chore: Template EdgeLB package version
    Signed-off-by: Simão Reis <sreis@mesosphere.io>

--- make/edgelb.mk
+++ make/edgelb.mk
@@ -4,12 +4,15 @@ EDGELB_PACKAGE_NAME := edgelb
 # EDGELB_SERVICE_ACCOUNT_NAME sets a default service account name for edgelb
 EDGELB_SERVICE_ACCOUNT_NAME := edgelb-principal
 
+# EDGELB_PACKAGE_VERSION sets the required package version
+EDGELB_PACKAGE_VERSION := v1.3.1-235-g50a400a
+
 .PHONY: edgelb.package.install
 edgelb.package.install: dcos.setup-security.edgelb
 	@dcos package repo remove edgelb-pool-aws || true
 	@dcos package repo remove edgelb-aws || true
-	@dcos package repo add --index=0 edgelb-aws https://edge-lb-infinity-artifacts.s3.amazonaws.com/saved/v1.3.1-235-g50a400a/edgelb/stub-universe-edgelb.json
-	@dcos package repo add --index=0 edgelb-pool-aws https://edge-lb-infinity-artifacts.s3.amazonaws.com/saved/v1.3.1-235-g50a400a/edgelb-pool/stub-universe-edgelb-pool.json
+	@dcos package repo add --index=0 edgelb-aws https://edge-lb-infinity-artifacts.s3.amazonaws.com/saved/${EDGELB_PACKAGE_VERSION}/edgelb/stub-universe-edgelb.json
+	@dcos package repo add --index=0 edgelb-pool-aws https://edge-lb-infinity-artifacts.s3.amazonaws.com/saved/${EDGELB_PACKAGE_VERSION}/edgelb-pool/stub-universe-edgelb-pool.json
 ifeq ($(SECURITY), strict)
 	@dcos package install --yes $(EDGELB_PACKAGE_NAME) --options=$(CURDIR)/hack/edgelb/package-options.json
 else
