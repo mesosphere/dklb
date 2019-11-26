bbbf55b33eb64b7f81ef1a6ee9c4ea7e2f1aa788
Simão Reis <sreis@mesosphere.io>
2019-11-26 08:53:39 -0100

    bugfix: Fix e2e status code check
    Signed-off-by: Simão Reis <sreis@mesosphere.io>

--- test/e2e/ingress_test.go
+++ test/e2e/ingress_test.go
@@ -1165,10 +1165,9 @@ var _ = Describe("Ingress", func() {
 
 			// Make sure that we can obtain a response from the Kubernetes API.
 			// This guarantees that HTTPS backends are supported.
-			status, body, err := f.Request("GET", publicIP, "/kubernetes")
+			status, _, err := f.Request("GET", publicIP, "/kubernetes")
 			Expect(err).NotTo(HaveOccurred(), "failed to perform http request")
-			Expect(status).To(Equal(http.StatusUnauthorized), "the response's status code doesn't match the expectation")
-			Expect(body).To(MatchRegexp(`{"kind":"Status","apiVersion":"v1","metadata":{},"status":"Failure","message":"Unauthorized","reason":"Unauthorized","code":401}`), "the response's body doesn't match the expectations")
+			Expect(status).Should(Or(Equal(http.StatusUnauthorized), Equal(http.StatusForbidden)))
 
 			// Undo the change made to the "default/kubernetes" service.
 			svc, err = f.KubeClient.CoreV1().Services(svc.Namespace).Get(svc.Name, metav1.GetOptions{})
